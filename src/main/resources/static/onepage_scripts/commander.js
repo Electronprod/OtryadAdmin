ee8b8f164376fef1e0504a316257232e31decfd0
Electron_prod <80621922+Electronprod@users.noreply.github.com>
2025-07-16 22:07:22 +0300

    Commander mark engine - work save

--- src/main/resources/static/onepage_scripts/commander.js
+++ src/main/resources/static/onepage_scripts/commander.js
@@ -1,7 +1,7 @@
 // Loading dependencies
-loadScript("/assets/mark_engine.js", null);
+loadScript("/assets/search_table.js", null);
 // Setting current date
-document.getElementById("dateField").valueAsDate = new Date()
+document.getElementById("dateField").valueAsDate = new Date();
 // Check all checkboxes
 document.getElementById('select_all_humans_checkbox').addEventListener('change', function() {
 	const checkboxes = document.querySelectorAll('input[name="select"]');
@@ -14,36 +14,28 @@ document.getElementById('select_all_humans_checkbox').addEventListener('change',
 		}
 	});
 });
-//Search
-function filterTable() {
-	const input = document.getElementById('searchInput');
-	const filter = input.value.toLowerCase();
-	const table = document.getElementById('markTable');
-	const tbody = table.getElementsByTagName('tbody')[0];
-	const rows = tbody.getElementsByTagName('tr');
-
-	for (let i = 0; i < rows.length; i++) {
-		const lastname = rows[i].getElementsByTagName('td')[1];
-		const firstname = rows[i].getElementsByTagName('td')[2];
-
-		if (lastname || firstname) {
-			const lastnameText = lastname.textContent.toLowerCase();
-			const firstnameText = firstname.textContent.toLowerCase();
-
-			if (lastnameText.indexOf(filter) > -1 || firstnameText.indexOf(filter) > -1) {
-				rows[i].style.display = '';
-			} else {
-				rows[i].style.display = 'none';
+document.addEventListener('DOMContentLoaded', function() {
+	hideColumn("markTable", 3);
+	const checkboxes = document.querySelectorAll('.custom-checkbox');
+	checkboxes.forEach(checkbox => {
+		checkbox.addEventListener('change', function() {
+			const index = this.id.split('-')[1];
+			const selectInput = document.getElementById('details-' + index);
+			if (selectInput) {
+				selectInput.disabled = this.checked;
 			}
-		}
-	}
-}
-//Search
-document.getElementById("searchInput").addEventListener("keypress", function() {
-	this.scrollIntoView(true);
+		});
+	});
 });
 // -------------Send data-------------
 async function send() {
+	if (isAnyColumnHidden) {
+		positive_send();
+	} else {
+		normal_send();
+	}
+}
+async function positive_send() {
 	try {
 		const presentPeople = [];
 		// For every checkbox...
@@ -59,7 +51,6 @@ async function send() {
 		var event_type = document.getElementById('event_type');
 		if (!event_type || event_type.value === "") {
 			showError("Не найдено название события!");
-			//event_type.scrollIntoView({ block: "center", behavior: "smooth" });
 			return;
 		}
 		const data_to_send = {
@@ -71,5 +62,51 @@ async function send() {
 	} catch (error) {
 		console.error("Error in send(): ", error);
 		showError("Произошла неизвестная ошибка!");
+	}
+}
+async function normal_send() {
+	try {
+		const unpresentPeople = [];
+		const presentPeople = [];
+		const checkboxes = document.querySelectorAll('.custom-checkbox');
+		checkboxes.forEach(checkbox => {
+			if (!checkbox.checked) {
+				const row = checkbox.closest('tr');
+				const reasonSelect = row ? row.querySelector('.details-input') : null;
+				if (reasonSelect) {
+					if (reasonSelect.value != "do_not_mark_user") {
+						unpresentPeople.push(JSON.stringify(
+							{
+								id: checkbox.value,
+								reason: reasonSelect.value
+							}
+						));
+					}
+				}
+			} else {
+				presentPeople.push(checkbox.value);
+			}
+		});
+		var event_type = document.getElementById('event_type');
+		if (!event_type || event_type.value === "") {
+			showError("Не найден тип события!");
+			return;
+		}
+		const data_to_send = {
+			presentPeople: presentPeople,
+			unpresentPeople: unpresentPeople,
+			event: event_type.value,
+			date: document.getElementById("dateField").value
+		};
+		sendData("/commander/mark_with_reasons", data_to_send);
+	} catch (error) {
+		showError("Произошла неизвестная ошибка!", String(error));
+	}
+}
+function toggleColumn(selectElement) {
+	if (selectElement.value == "true") {
+		showColumn("markTable", 3);
+	} else {
+		hideColumn("markTable", 3);
 	}
 }
